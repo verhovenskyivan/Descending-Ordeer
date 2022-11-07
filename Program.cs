3180344c4c892d7cc1e3f873a499ac7aac146a24
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-07 23:26:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int DescendingOrder(int num)
+    {
+
+        return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
+    }
+}
